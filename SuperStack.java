db072dbe86f5ab34e049a73c057fcbfa9604a4fd
thuantran1 <70294185+thuantran1@users.noreply.github.com>
2020-10-21 17:21:24 -0500

    Implement SuperStack

---
+++ SuperStack.java
@@ -0,0 +1,35 @@
+
+public class SuperStack<T> {
+	private T[] items;
+	private int top;
+	
+	public SuperStack(int size){
+		items = (T[]) new Object[size];
+	}
+	
+	public boolean isEmpty() {
+		return top == 0;
+	}
+	
+	public boolean isFull() {
+		return top == items.length;
+	}
+	
+	public boolean push(T item) {
+		if(isFull()) {
+			return false;
+		}
+		
+		items[top++] = item;
+		return true;
+	}
+	
+	public T pop() {
+		if(isEmpty())
+			return null;
+		else {
+			top--;
+			return items[top];
+		}
+	}
+}
